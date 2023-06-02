94550387ddd1b82ea2d9f6008c0c351dfa350e6e
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-06-01 21:23:03 -0400

    first commit

--- src/main/java/com/myorg/HelloCdkStack.java
+++ src/main/java/com/myorg/HelloCdkStack.java
@@ -1,8 +1,14 @@
 package com.myorg;
 
 import software.constructs.Construct;
+
+import java.util.Arrays;
+
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecr.LifecycleRule;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecr.TagMutability;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
 
@@ -16,9 +22,14 @@ public class HelloCdkStack extends Stack {
 
         // The code that defines your stack goes here
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "HelloCdkQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+
+        Repository repo = Repository.Builder.create(this, "repository")
+        		.repositoryName("hello-world-repository")
+        		.imageScanOnPush(Boolean.TRUE)
+        		.lifecycleRules(Arrays.asList(LifecycleRule.builder()
+        				.maxImageCount(10)
+        				.build()))
+        		.imageTagMutability(TagMutability.IMMUTABLE)
+        		.build();
     }
 }
